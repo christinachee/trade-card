2d11bb401caa76f218d52790d4c50bd55772665c
Christina Chee <waichee723@gmail.com>
2024-06-17 17:40:34 +0100

    initial commit

---
+++ src/pages/Campaign.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const Card = (props) => {
+    const { user: tradeCard } = props;
+
+    return (
+        <div>
+            <div>Image ID: {tradeCard.id}</div>
+            <div>Image Link: <a href={tradeCard.frontimage_thumbnail}>{tradeCard.frontimage_thumbnail}</a></div>
+            <div>Video Link: <a href={tradeCard.frontimage}>{tradeCard.frontimage}</a></div>
+            <img src={tradeCard.frontimage_thumbnail} />
+            <hr />
+        </div>
+    );
+};
+
+export default function Campaign() {
+
+    const { id } = useParams();
+
+    const [tradeCards, setTradeCards] = useState([]);
+
+    useEffect(() => {
+        const url = `https://api-helloproject.orical.jp/cardpacks/${id}`;
+
+        console.log(url)
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                // const { results } = json;
+                // Only put the results in state, ie, the actual users array
+                setTradeCards(json.cards);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {tradeCards.map((tradeCard) => (
+                <Card key={tradeCard.id} user={tradeCard} />
+            ))}
+        </div>
+    );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import "./Home.css";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Card = (props) => {
+  const { user: campaign } = props;
+
+  return (
+    <div>
+      <div>
+        <Link to={`/campaign/${campaign.id}`}>
+          {campaign.id} - {campaign.name}
+        </Link>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default function App() {
+  const [campaigns, setCampaigns] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://api-helloproject.orical.jp/cardpacks?partner_id=13&page=1&return_tutorial_pack=false";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        // const { results } = json;
+        // Only put the results in state, ie, the actual users array
+        setCampaigns(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {campaigns.map((campaign) => (
+        <Card key={campaign.id} user={campaign} />
+      ))}
+    </div>
+  );
+}
